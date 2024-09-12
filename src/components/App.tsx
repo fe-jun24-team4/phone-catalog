41a3c3b5745d8fb551b08d22a69217843af54d3b
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-09-13 00:45:33 +0200

    root-styles

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="header">Header</header>
+
+      <main className="page">
+        <div className="page___container">Content</div>
+      </main>
+
+      <footer className="footer">
+        <div className="footer__container">Footer</div>
+        </footer>
+    </div> 
+  );
+}
+
+export default App;
