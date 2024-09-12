5d9ec84f2dea85a76165a2440348d1ee9b5a256b
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-09-13 01:44:02 +0200

    primary-buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,12 @@ function App() {
       <header className="header">Header</header>
 
       <main className="page">
-        <div className="page___container">Content</div>
+        <div className="page___container" style={{width: 350}}>Content
+        <div className="test" style={{display: "flex", gap: 15, alignItems: 'center'}}>
+        <button className="button-primary">button</button>
+        <button className="button-round button-round--heart button-round--heart--selected"></button>
+        </div>
+        </div>
       </main>
 
       <footer className="footer">
