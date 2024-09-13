9bf683f0561424834b8be37b6ce1755d46d7dccb
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-09-13 14:54:43 +0200

    renamed router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-//import App from './components/app/App'
 import './index.scss';
-import { RootPage } from './components/pages/RootPage';
+import { RootPage } from './components/pages/RootMap';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
