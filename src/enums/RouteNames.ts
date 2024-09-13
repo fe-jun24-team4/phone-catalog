abf2cce74ac9e8dabe3648f554b0040217c9e934
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-09-13 14:40:13 +0200

    feature-router

---
+++ src/enums/RouteNames.ts
@@ -0,0 +1,7 @@
+export enum RouteNames {
+    home = '/',
+    phones = 'phones',
+    tablets = 'tablets',
+    accessories = 'accessories',
+    pageNotFound = '*'
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
+//import App from './components/app/App'
 import './index.scss';
+import { RootPage } from './components/pages/RootPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootPage />
   </StrictMode>,
 );
