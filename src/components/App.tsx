14c52f0e755cec220964e1c492d41a612f79002a
Yurii <uaremko945@gmail.com>
2024-09-18 23:20:42 +0300

    about section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
-import { Outlet } from 'react-router-dom';
+import AboutSection from './AboutSection/AboutSection';
+
 
 function App() {
+
+  const description = [
+      {
+        title: "And then there was Pro",
+        text: [
+          "A transformative triple-camera system that adds tons of capability without complexity.",
+          "An unprecedented leap in battery life. And a mind-blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro."
+        ]
+      },
+      {
+        title: "Camera",
+        text: [
+          "Meet the first triple-camera system to combine cutting-edge technology with the legendary simplicity of iPhone. Capture up to four times more scene. Get beautiful images in drastically lower light. Shoot the highest-quality video in a smartphone — then edit with the same tools you love for photos. You’ve never shot with anything like it."
+        ]
+      },
+      {
+        title: "Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it. Love it.",
+        text: [
+          "iPhone 11 Pro lets you capture videos that are beautifully true to life, with greater detail and smoother motion. Epic processing power means it can shoot 4K video with extended dynamic range and cinematic video stabilization — all at 60 fps. You get more creative control, too, with four times more scene and powerful new editing tools to play with."
+        ]
+      }
+    ];
+
   return (
     <div className="wrapper">
       <header className="header">
@@ -12,7 +36,7 @@ function App() {
 
       <main className="page">
         <div className="page___container">
-          <Outlet />
+          <AboutSection description={description} />
         </div>
       </main>
 
