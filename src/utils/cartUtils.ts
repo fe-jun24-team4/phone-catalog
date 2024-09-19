b97bb7dad88d8161502b7c3bf83c195e2f736038
Teollan <serhiikofficial@gmail.com>
2024-09-19 18:09:24 +0300

    changed types for cart

--- src/utils/cartUtils.ts
+++ src/utils/cartUtils.ts
@@ -1,4 +1,5 @@
-import { CartItem, CartState } from '../../src/types/Cart';
+import { CartState } from '../../src/types/Cart';
+import { OrderItem } from '../types/OrderItem';
 
 import data from '../api/phones.json';
 
@@ -10,13 +11,13 @@ export const getInitialCartState = (): CartState => {
     : data.slice(0, 3).map((product, index) => ({ product, amount: index + 1 }));
 };
 
-export const addItemToCart = (prevCart: CartState, item: CartItem): CartState => {
-  const existingItem = prevCart.find(cartItem => cartItem.id === item.id);
+export const addItemToCart = (prevCart: CartState, item: OrderItem): CartState => {
+  const existingItem = prevCart.find(cartItem => cartItem.product.id === item.product.id);
 
   if (existingItem) {
     return prevCart.map(cartItem =>
-      cartItem.id === item.id
-        ? { ...cartItem, quantity: cartItem.quantity + item.quantity }
+      cartItem.product.id === item.product.id
+        ? { ...cartItem, quantity: cartItem.amount + item.amount }
         : cartItem,
     );
   }
