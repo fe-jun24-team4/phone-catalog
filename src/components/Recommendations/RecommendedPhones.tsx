7fe68972b7dfcb77bf6ffb8731e88ccfb6ceb37b
piyopiyo0 <tetyana0204@gmail.com>
2024-09-19 12:57:01 +0300

    shuffleArray.ts, useFetchRecommended.ts, conditional rendering to a final return are added

--- src/components/Recommendations/RecommendedPhones.tsx
+++ src/components/Recommendations/RecommendedPhones.tsx
@@ -1,57 +1,38 @@
 import React, { useMemo } from 'react';
-import { useFetchData } from '../../hooks/useFetch';
-
-interface Phone {
-  id: string;
-  name: string;
-  category: string;
-  rating: number;
-}
-
-const shuffleArray = (array: Phone[]): Phone[] => {
-  const shuffled = [...array];
-
-  // тасування Фішера-Йейтса для рандому
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-
-  return shuffled;
-};
+import { useFetchRecommended } from '../../hooks/useFetchRecommended';
+import { shuffleArray } from '../../utils/shuffleArray';
+import { Product } from '../../types/Product';
 
 export const RecommendedPhones: React.FC = () => {
-  const { data: phones, isLoading, isError } = useFetchData<Phone>('api/phones.json');
+  const { data: phones, isLoading, isError } = useFetchRecommended<Product[]>('api/phones.json');
 
-  if (isLoading) {
-    return <div>Loading recommendations...</div>;
-  }
+  const recommendedPhones = useMemo(() => {
+    if (!phones) {
+      return [];
+    }
 
-  if (isError) {
-    return <div>Failed to load recommendations...</div>;
-  }
+    const shuffledPhones = shuffleArray(phones);
 
-  // useMemo викликається тільки якщо дані успішно завантажені, але у цьому випадку лінтер свариться.
-  // Якщо поставити обробку помилок нижче, помилка зникне (але наскільки правильно її туди ставити?).
-  // Можу взагалі видалити мемоїзацію, можливо вона тут зайва.
-  // Поки відключила правило до моменту вирішення питання
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const shuffledPhones = useMemo(() => shuffleArray(phones), [phones]);
-  const recommendedPhones = shuffledPhones.slice(0, 5);
+    return shuffledPhones.slice(0, 5);
+  }, [phones]);
 
   return (
     <div>
-      <h2>Recommended Phones</h2>
-      <ul>
-        {recommendedPhones.map(phone => (
-          <li key={phone.id}>
-            <h3>{phone.name}</h3>
-            <p>Category: {phone.category}</p>
-            <p>Rating: {phone.rating}</p>
-          </li>
-        ))}
-      </ul>
+      {isLoading && <div>Loading recommendations...</div>}
+      {isError && <div>Failed to load recommendations...</div>}
+      {!isLoading && !isError && (
+        <>
+          <h2>Recommended Phones</h2>
+          <ul>
+            {recommendedPhones.map(phone => (
+              <li key={phone.id}>
+                <h3>{phone.name}</h3>
+                <p>Category: {phone.category}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
