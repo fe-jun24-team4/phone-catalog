d1d0d01443d1c142d7734da57fb3d796b0636f70
Teollan <serhiikofficial@gmail.com>
2024-09-24 22:08:42 +0300

    CartItem is now a link

--- src/pages/CartPage/components/CartItem/CartItem.tsx
+++ src/pages/CartPage/components/CartItem/CartItem.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { OrderItem } from '../../../../types/OrderItem';
 import { useCartContext } from '../../context/CartContext';
 import { ButtonRounded } from '../../../../components/buttons';
+import { Link } from 'react-router-dom';
 
 type CartItemProps = {
   item: OrderItem;
@@ -17,7 +18,10 @@ export const CartItem = ({ item }: CartItemProps) => {
 
   const { removeItem, updateQuantity } = useCartContext();
 
-  const handleRemove = () => {
+  const handleRemove = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
     removeItem(product.itemId);
   };
 
@@ -28,35 +32,47 @@ export const CartItem = ({ item }: CartItemProps) => {
   };
 
   return (
-    <div className={styles.cart}>
-      <div className={styles.container}>
-        <div className={styles.position}>
-          <span className="icon-plus" onClick={handleRemove} />
-          <div className={styles.photo}>
-            <img src={image} alt={name} className={styles.photoImg} />
+    <Link to={`/${product.category}/${product.itemId}`} className={styles.card}>
+      <div className={styles.cart}>
+        <div className={styles.container}>
+          <div className={styles.position}>
+            <span className="icon-plus" onClick={handleRemove} />
+            <div className={styles.photo}>
+              <img src={image} alt={name} className={styles.photoImg} />
+            </div>
+            <div className={styles.details}>
+              <p className={styles.description}>{name}</p>
+            </div>
           </div>
-          <div className={styles.details}>
-            <p className={styles.description}>{name}</p>
-          </div>
-        </div>
 
-        <div className={classNames(styles.position, styles.spaceBetween)}>
-          <div className={styles.quantityBlock}>
-            <ButtonRounded
-              icon="icon-minus"
-              disabled={amount === 1}
-              onClick={() => handleQuantityChange(amount - 1)}
-            />
-            <p className={styles.quantity}>{amount}</p>
-            <ButtonRounded
-              icon="icon-plus"
-              disabled={amount >= 9}
-              onClick={() => handleQuantityChange(amount + 1)}
-            />
+          <div className={classNames(styles.position, styles.spaceBetween)}>
+            <div className={styles.quantityBlock}>
+              <ButtonRounded
+                icon="icon-minus"
+                disabled={amount === 1}
+                onClick={event => {
+                  event.preventDefault();
+                  event.stopPropagation();
+
+                  handleQuantityChange(amount - 1);
+                }}
+              />
+              <p className={styles.quantity}>{amount}</p>
+              <ButtonRounded
+                icon="icon-plus"
+                disabled={amount >= 9}
+                onClick={event => {
+                  event.stopPropagation();
+                  event.preventDefault();
+
+                  handleQuantityChange(amount + 1);
+                }}
+              />
+            </div>
+            <p className={styles.price}>${price}</p>
           </div>
-          <p className={styles.price}>${price}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
